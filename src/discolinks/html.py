f95974b64e7000092c21ebe1e754e7bd70de8ea6
Bertrand Bonnefoy-Claudet <bertrand@bertrandbc.com>
2022-05-30 16:29:40 +0200

    Make dedicated HTML link parsing code and test it

---
+++ src/discolinks/html.py
@@ -0,0 +1,44 @@
+from typing import Sequence
+from urllib.parse import urldefrag, urljoin, urlparse
+
+from requests_html import HTML
+
+from .core import Link, LinkOrigin
+
+
+def get_hrefs(body: str) -> Sequence[str]:
+    html = HTML(html=body)
+
+    return [url for a in html.find("a") if (url := a.attrs.get("href")) is not None]
+
+
+def parse_href(href: str, base_link: Link) -> Link:
+    """
+    Parse the value of an `href` HTML attribute into a URL.
+
+    If the link is relative, we need the base URL to infer the absolute URL.
+    """
+
+    (href, _) = urldefrag(href)
+    parsed = urlparse(href)
+
+    if parsed.scheme:
+        scheme = parsed.scheme
+    else:
+        scheme = base_link.scheme
+
+    if parsed.netloc:
+        netloc = parsed.netloc
+        url = href
+    else:
+        netloc = base_link.netloc
+        url = urljoin(base_link.url, href)
+
+    return Link(url=url, scheme=scheme, netloc=netloc)
+
+
+def get_links(body: str, link: Link) -> Sequence[tuple[Link, LinkOrigin]]:
+    return [
+        (parse_href(href, base_link=link), LinkOrigin(href=href, page=link))
+        for href in get_hrefs(body)
+    ]
---
+++ tests/test_html.py
@@ -0,0 +1,44 @@
+import pytest
+
+from discolinks.core import Link, LinkOrigin
+from discolinks.html import get_hrefs, parse_href
+
+
+@pytest.mark.parametrize(
+    "body,expected",
+    [
+        ("<body></body>", []),
+        ("""<a href="foo">""", ["foo"]),
+        ("""<a href="foo#bar">""", ["foo#bar"]),
+    ],
+)
+def test_get_hrefs(body: str, expected: list[tuple[Link, LinkOrigin]]):
+    result = get_hrefs(body=body)
+
+    assert list(result) == expected
+
+
+@pytest.mark.parametrize(
+    "href,base_link,expected",
+    [
+        (
+            "",
+            Link.from_url("http://example.net"),
+            Link.from_url("http://example.net"),
+        ),
+        (
+            "foo",
+            Link.from_url("http://example.net"),
+            Link.from_url("http://example.net/foo"),
+        ),
+        (
+            "http://example.org",
+            Link.from_url("http://example.net"),
+            Link.from_url("http://example.org"),
+        ),
+    ],
+)
+def test_parse_href(href: str, base_link: Link, expected: Link):
+    result = parse_href(href=href, base_link=base_link)
+
+    assert result == expected
